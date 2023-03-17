fd09fcb7bff0987e9204dd024214e559dbfacb74
Minnathullah-TheMysterious <minnathullahmohammed@gmail.com>
2023-03-17 16:12:57 +0530

    post refactoring with comments

--- src/App.js
+++ src/App.js
@@ -1,64 +1,142 @@
-import './App.css';
-import React, { Component } from 'react'
-import Navbar from './components/Navbar';
-import News from './components/News';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
-  constructor(){
-    super()
-    this.state={
-      mode:'light',
-      text:'black',
-    }
+  constructor() {
+    super();
+    this.state = {
+      mode: "light",
+      text: "black",
+    };
   }
 
-  handleToggleMode=()=>{
-    console.log('toggleMode')
-    if(this.state.mode === 'light'){
+  handleToggleMode = () => {
+    // console.log("toggleMode");
+    if (this.state.mode === "light") {
       this.setState({
-        mode: 'dark',
-        text:'white',
-      })
-      // document.body.style.backgroundColor = 'black'
-      // document.body.style.color = 'white'
-      // for(let i = 0; i<39; i++){
-      //   document.body.getElementsByClassName('card')[i].style.backgroundColor='black'
-      // }
-    }else{
+        mode: "dark",
+        text: "white",
+      });
+    } else {
       this.setState({
-        mode: 'light',
-        text:'black'
-      })
-      // document.body.style.backgroundColor = 'white'
-      // document.body.style.color = 'black'
-      // for(let i = 0; i<39; i++){
-      //   document.body.getElementsByClassName('card')[i].style.backgroundColor='white'
-      // }
+        mode: "light",
+        text: "black",
+      });
     }
-  }
+  };
 
+  pageSize = 6;
+  apiKey = "45890530df224bbab7fded0071ee7eac";
+  country = "in";
 
   render() {
     return (
       <>
         <Router>
-          <Navbar title="NewsMonkey" mode={this.state.mode} toggleMode={this.handleToggleMode} text={this.state.text}/>
+          <Navbar
+            title="NewsMonkey"
+            mode={this.state.mode}
+            toggleMode={this.handleToggleMode}
+            text={this.state.text}
+          />
           <Routes>
-            <Route exact path='/' element={<News pageSize={6} country="in" category="general" apiKey="45890530df224bbab7fded0071ee7eac" key="general"/>}></Route> //a unique key is required for remounting the componenet
-            <Route exact path='/business' element={<News pageSize={6} country="in" category="business" apiKey="45890530df224bbab7fded0071ee7eac" key="business"/>}></Route>
-            <Route exact path='/entertainment' element={<News pageSize={6} country="in" category="entertainment" apiKey="45890530df224bbab7fded0071ee7eac" key="entertainment"/>}></Route>
-            <Route exact path='/health' element={<News pageSize={6} country="in" category="health" apiKey="45890530df224bbab7fded0071ee7eac" key="health"/>}></Route>
-            <Route exact path='/science' element={<News pageSize={6} country="in" category="science" apiKey="45890530df224bbab7fded0071ee7eac" key="science"/>}></Route>
-            <Route exact path='/sports' element={<News pageSize={6} country="in" category="sports" apiKey="45890530df224bbab7fded0071ee7eac" key="sports"/>}></Route>
-            <Route exact path='/technology' element={<News pageSize={6} country="in" category="technology" apiKey="45890530df224bbab7fded0071ee7eac" key="technology"/>}></Route>
+            <Route
+              exact
+              path="/"
+              element={
+                <News
+                  pageSize={this.pageSize}
+                  country={this.country}
+                  category="general"
+                  apiKey={this.apiKey}
+                  key="general" //a unique key is required for remounting the componenet
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/business"
+              element={
+                <News
+                  pageSize={this.pageSize}
+                  country={this.country}
+                  category="business"
+                  apiKey={this.apiKey}
+                  key="business"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/entertainment"
+              element={
+                <News
+                  pageSize={this.pageSize}
+                  country={this.country}
+                  category="entertainment"
+                  apiKey={this.apiKey}
+                  key="entertainment"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/health"
+              element={
+                <News
+                  pageSize={this.pageSize}
+                  country={this.country}
+                  category="health"
+                  apiKey={this.apiKey}
+                  key="health"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/science"
+              element={
+                <News
+                  pageSize={this.pageSize}
+                  country={this.country}
+                  category="science"
+                  apiKey={this.apiKey}
+                  key="science"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/sports"
+              element={
+                <News
+                  pageSize={this.pageSize}
+                  country={this.country}
+                  category="sports"
+                  apiKey={this.apiKey}
+                  key="sports"
+                />
+              }
+            ></Route>
+            <Route
+              exact
+              path="/technology"
+              element={
+                <News
+                  pageSize={this.pageSize}
+                  country={this.country}
+                  category="technology"
+                  apiKey={this.apiKey}
+                  key="technology"
+                />
+              }
+            ></Route>
           </Routes>
         </Router>
       </>
-    )
+    );
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,7 +8,7 @@ export class NewsItem extends Component {
       <div>
         <div className="card my-4">
           <span
-            class="position-absolute top-0 translate-middle badge rounded-pill bg-success"
+            className="position-absolute top-0 translate-middle badge rounded-pill bg-success"
             style={{ zIndex: 1, left: "90%" }}
           >
             {source}
@@ -18,7 +18,7 @@ export class NewsItem extends Component {
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
             <p className="card-text">
-              <small className="text-muted">
+              <small className="text-danger">
                 By "{author ? author : "Unknown"}" on{" "}
                 {new Date(date).toGMTString()}
               </small>
